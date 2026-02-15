2025c140128162f2ef3afb61c16a5a95453576f4
Hericles <hericlesreal@gmail.com>
2026-02-15 19:44:13 -0300

    Adicionar arquivos de projeto.

---
+++ BlacklistApi/Controllers/BlacklistController.cs
@@ -0,0 +1,29 @@
+﻿using BlacklistApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlacklistApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class BlacklistController : ControllerBase
+{
+    private static List<Blacklist> blacklists = new List<Blacklist>();
+
+    [HttpPost]
+    public void AddBlacklist([FromBody] Blacklist blacklist)
+    {
+        blacklists.Add(blacklist);
+    }
+
+    [HttpGet]
+    public IEnumerable<Blacklist> GetBlacklist()
+    {
+        return blacklists;  
+    }
+
+    [HttpDelete]
+    public void DeleteBlacklist(Blacklist blacklist)
+    {
+        blacklists.Remove(blacklist);
+    }
+}
---
+++ BlacklistApi/Models/Blacklist.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlacklistApi.Models;
+
+public class Blacklist
+{
+    [Required(ErrorMessage = "O nome não pode ser vazio")]
+    [MaxLength(100)]
+    public string Nome { get; set; } = string.Empty;
+    [Required(ErrorMessage = "Um corredor deve possuir um carro")]
+    [MaxLength(100)]
+    public string Carro { get; set; } = string.Empty;
+    [Required]
+    [Range(1, 15, ErrorMessage = "A posição vai de 15 até 1. Verifique e tente novamente")]
+    public int Posicao { get; set; }
+}
