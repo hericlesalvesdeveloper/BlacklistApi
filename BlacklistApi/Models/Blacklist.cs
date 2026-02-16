fe955bae0c22f26779b4431e6fad3f35de5e211f
Hericles <hericlesreal@gmail.com>
2026-02-16 13:33:57 -0300

    Refatorado a model

--- BlacklistApi/Models/Blacklist.cs
+++ BlacklistApi/Models/Blacklist.cs
@@ -4,14 +4,9 @@ namespace BlacklistApi.Models;
 
 public class Blacklist
 {
-    [Required(ErrorMessage = "O nome não pode ser vazio")]
-    [MaxLength(100)]
-    public string Nome { get; set; } = string.Empty;
-    [Required(ErrorMessage = "Um corredor deve possuir um carro")]
-    [MaxLength(100)]
-    public string Carro { get; set; } = string.Empty;
-    [Required]
-    [Range(1, 15, ErrorMessage = "A posição vai de 15 até 1. Verifique e tente novamente")]
-    public int Posicao { get; set; }
+    public Guid Id { get; set; }
+    public string CarName { get; set; }
+    public string Reason { get; set; }
+    public DateTime CreatedAt { get; set; }
 }
 
