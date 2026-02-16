1479235dcb324305331399fc3441ca49e4e30b0e
Hericles <hericlesreal@gmail.com>
2026-02-16 13:31:49 -0300

    Cria DTOs de Resposta, Criação e Atuialização

---
+++ BlacklistApi/DTOs/BlacklistResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace BlacklistApi.DTOs;
+
+public class BlacklistResponse
+{
+    public Guid Id { get; set; }
+    public string CarName { get; set; }
+    public string Reason { get; set; }
+    public DateTime CreatedAt { get; set; }
+
+}
---
+++ BlacklistApi/DTOs/CreateBlacklistRequest.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlacklistApi.DTOs;
+
+public class CreateBlacklistRequest
+{
+    public string CarName { get; set; }
+    public string Reason { get; set; }
+}
